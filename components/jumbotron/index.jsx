d2c47914e72d81f6d121ea7b9c2077d835992d27
naufal <abnormalchild414@gmail.com>
2022-12-21 13:54:29 +0700

    minor fix

--- components/jumbotron/index.jsx
+++ components/jumbotron/index.jsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 const Jumbotron = () => {
   return (
     <section className="lg:px-24 px-12 lg:mt-0 mt-[70%]">
-      <h1 className="text-white font-semibold lg:text-7xl text-4xl w-1/2">Decentralized Protocol Exchange and AMM</h1>
+      <h1 className="text-white font-semibold lg:text-7xl text-4xl w-1/2">Decentralized Perpetual Exchange and AMM</h1>
       <p className="font-semibold text-white my-4 lg:my-8">Powered by Aptos</p>
       <Image src="/launch-app-button.svg" alt="launch app button" width={350} height={150} className="hidden lg:block cursor-pointer ml-[-23px]" />
       <Image src="/launch-app-button.svg" alt="launch app button" width={150} height={50} className="lg:hidden block cursor-pointer ml-[-23px]" />
--- pages/index.js
+++ pages/index.js
@@ -27,23 +27,23 @@ export default function Home() {
         <div className="my-10 h-[114px] flex flex-row justify-between mx-auto bg-[#02AF91]">
           <div className="mx-auto my-auto w-1/2">
             <p className="text-white text-center font-semibold text-sm lg:text-2xl">Utility and Governance</p>
-            <p className="text-white text-center font-semibold text-xs lg:text-2xl">20% occures 30% fees</p>
+            <p className="text-white text-center font-semibold text-xs lg:text-2xl">20% accures 30% fees</p>
           </div>
           <Image src='/Divider.svg' className="my-4" height={10} width={10} alt='divider' />
           <div className="mx-auto my-auto w-1/2">
             <p className="text-white text-center font-semibold text-sm lg:text-2xl">Liquidity Token</p>
-            <p className="text-white text-center font-semibold text-xs lg:text-2xl">65% occures 70% fees</p>
+            <p className="text-white text-center font-semibold text-xs lg:text-2xl">65% accures 70% fees</p>
           </div>
         </div>
         <div className="flex lg:flex-row flex-col lg:space-x-6 space-y-6">
           <h1 className="font-semibold text-3xl lg:text-6xl text-white w-full lg:w-1/3">Why Aptos</h1>
           <p className=" lg:font-semibold text-lg text-white w-full lg:w-2/3">Aptos is a new layer one blockchain that uses the move language, parallel execution and other innovations to enable incredibly fast infrastructure for mass web3 adoption</p>
         </div>
-        <div className="flex lg:flex-row flex-col lg:space-x-6 space-y-3 mt-10">
+        <div className="flex lg:flex-row flex-col lg:space-x-6 lg:space-y-0 space-y-3 mt-10">
         {
           data.map((item, index) => {
             return (
-              <div key={index} className='bg-white bg-opacity-10 flex flex-row items-center w-full lg:w-1/3 justify-center py-6 border border-white rounded-lg space-x-5'>
+              <div key={index} className='bg-white bg-opacity-10 flex flex-row items-center w-full lg:w-1/3 h-full justify-center py-6 border border-white rounded-lg space-x-5'>
                 <Image src={item.src} height={80} width={80} alt={item.title} />
                 <h1 className="font-semibold text-2xl text-white w-1/3">{item.title}</h1>
                 </div>
